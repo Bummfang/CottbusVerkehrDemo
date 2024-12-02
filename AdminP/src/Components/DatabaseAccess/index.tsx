6030db55951d64f27b76ad71b11cc20ba9c37fb6
BummfangTV <maximiliangornbw@web.de>
2024-12-02 18:43:57 +0100

    final fix realy final

--- AdminP/src/Components/DatabaseAccess/index.tsx
+++ AdminP/src/Components/DatabaseAccess/index.tsx
@@ -83,13 +83,12 @@ const DatabaseAccess = () =>{
                             </div>
                         </div>
                     </div>
-
                     {/* Buttons */}
                     <div className="flex justify-end space-x-4">
-                    <button className="bg-gray-500 font-semibold text-white px-6 py-2 rounded-md hover:bg-gray-600">
+                    <button className="bg-[#c93636] font-semibold text-white px-6 py-2 rounded-md hover:bg-gray-600">
                             Abbrechen
                         </button>
-                        <button className="bg-[#c93636] font-semibold text-white px-6 py-2 duration-75 rounded-md hover:bg-[#a12c2c]">
+                        <button className="bg-[#3bbe76] font-semibold text-white px-6 py-2 duration-75 rounded-md hover:bg-[#a12c2c]">
                             Speichern
                         </button>
                     </div>
