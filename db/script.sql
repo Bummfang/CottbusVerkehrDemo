75c6e1e5ca68ebf4505fc6e8783e1749af22d638
ottonils <ottonils@b-tu.de>
2024-12-04 18:56:41 +0100

    changed database

---
+++ db/script.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee (employee_id, name, password, admin_key)
+VALUES (2, 'Max', '123', '111');
+
+INSERT INTO employee (employee_id, name, password, admin_key)
+VALUES (3, 'Nils', '123', '000');
